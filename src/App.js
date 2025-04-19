a63fe45e40cda234f61e061ca4524feb6a383c0a
Matilda Söderberg <matilda.89@live.se>
2025-04-19 20:54:53 +0200

    Added new component Shoppinglist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import Shoppinglist from "./Shoppinglist";
 function App() {
   return (
     <>
-    {/* Code... */}
+    <Shoppinglist/>
     </>
   );
 }
---
+++ src/Shoppinglist.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Shoppinglist () {
+    return(
+        <>
+        </>
+    )
+}
