9cdb1912db77e61aaee0c302c4cc5a8743e32bd5
Matilda Söderberg <matilda.89@live.se>
2025-04-19 21:01:08 +0200

    Fixed problem with root being removed from html

--- src/Shoppinglist.js
+++ src/Shoppinglist.js
@@ -3,6 +3,14 @@ import React from 'react';
 export default function Shoppinglist () {
     return(
         <>
+        <ul>
+            <li>Test 1</li>
+            <li>Test 2</li>
+            <li>Test 3</li>
+            <li>Test 4</li>
+            <li>Test 5</li>
+            <li>Test 6</li>
+        </ul>
         </>
     )
 }
