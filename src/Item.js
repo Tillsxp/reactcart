542df349c8cba4bfc48a75b2898649fa26fbaba8
Matilda Söderberg <matilda.89@live.se>
2025-04-20 12:19:05 +0200

    SOC, moved json to List component

--- src/Item.js
+++ src/Item.js
@@ -1,40 +1,12 @@
 import React from 'react';
 
 export default function Item(props) {
-    const item = [
-        {
-            name:"Säbövik",
-            picture:"bed.png",
-            description: "A modern comfortable bed"
-        },
-        {
-            name:"Skogsta",
-            picture:"chair.png",
-            description: "Made out of acacia wood with back support"
-        },
-        {
-            name:"Kivik",
-            picture:"sofa.png",
-            description: "3-seatsofa comfortable sofa"
-        },
-        {
-            name:"Mörbylånga",
-            picture:"table.png",
-            description: "Oak table with a lot of character"
-        }
-    ];
-    const listItems = item.map(item =>
-        <li>
-        <h4>{item.name}</h4>
-        <img src={`./images/${item.picture}`}/>
-        <p>{item.description}</p>
-        </li>
-    )
+
     return(
-        <>
-            <ul>
-                {listItems}
-            </ul>
-        </>
+        <li>
+                <h4>{props.title}</h4>
+                <img src={`./images/${props.picture}`} alt={props.title}/>
+                <p>{props.description}</p>
+        </li>
     )
 }
--- src/Shoppinglist.js
+++ src/Shoppinglist.js
@@ -2,13 +2,42 @@ import React from 'react';
 import Item from './Item';
 
 export default function Shoppinglist () {
+    const item = [
+        {
+            name:"Säbövik",
+            picture:"bed.png",
+            description: "A modern comfortable bed"
+        },
+        {
+            name:"Skogsta",
+            picture:"chair.png",
+            description: "Made out of acacia wood with back support"
+        },
+        {
+            name:"Kivik",
+            picture:"sofa.png",
+            description: "3-seatsofa comfortable sofa"
+        },
+        {
+            name:"Mörbylånga",
+            picture:"table.png",
+            description: "Oak table with a lot of character"
+        }
+    ];
+    const listItems = item.map(item => 
+        <Item 
+        key={item.name}
+        title={item.name}
+        picture={item.picture}
+        description={item.description}
+        />
+    
+    );
     return(
         <>
         <h4>Ikea list</h4>
         <ul>
-            <li>
-                <Item/>
-            </li>
+            {listItems}
         </ul>
         </>
     )
