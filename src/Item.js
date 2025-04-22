c664686aed5c9940fcf09b9f9317485079190f55
Matilda Söderberg <matilda.89@live.se>
2025-04-22 08:06:25 +0200

    Added add button and logic for no less than 1 in amount

--- src/Item.js
+++ src/Item.js
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import './item.css';
 
 export default function Item(props) {
-const [amount, setAmount] = useState(0);
+const [amount, setAmount] = useState(1);
+
     return(
         <li>
                 <h4>{props.title}</h4>
                 <img src={`./images/${props.picture}`} alt={props.title}/>
                 <p>{props.description}</p>
                 <div className="amount-container"> {/* Seperate into a different component with state logic*/}
-                <button onClick={() => setAmount((a) =>  a - 1)}>-</button>
+                <button onClick={() => setAmount((a)  =>  (a > 1 ? a - 1 : a))}>-</button>
                 <p>{amount}</p>
-                <button onClick={() => setAmount((a)=> a + 1)}>+</button>
+                <button onClick={() => setAmount((a)=> (a + 1))}>+</button>
+                <button>Add to cart</button>
                 </div>
         </li>
     )
