dfdc54dcdb3e8b97c8db2f263c0cc121543781cb
Matilda Söderberg <matilda.89@live.se>
2025-04-22 07:32:45 +0200

    Added setState Amount to set amount of items

--- src/Item.js
+++ src/Item.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './item.css';
 
 export default function Item(props) {
-
+const [amount, setAmount] = useState(0);
     return(
         <li>
                 <h4>{props.title}</h4>
                 <img src={`./images/${props.picture}`} alt={props.title}/>
                 <p>{props.description}</p>
                 <div className="amount-container"> {/* Seperate into a different component with state logic*/}
-                <button>-</button>
-                <p>0</p>
-                <button>+</button>
+                <button onClick={() => setAmount((a) =>  a - 1)}>-</button>
+                <p>{amount}</p>
+                <button onClick={() => setAmount((a)=> a + 1)}>+</button>
                 </div>
         </li>
     )
